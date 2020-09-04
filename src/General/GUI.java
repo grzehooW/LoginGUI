3c44ea20e2a1538f5c36e490b863c71888622589
grzehoo <grzehoo.wrzosek@gmail.com>
2020-09-04 11:38:33 +0100

    Created Frame

--- src/General/GUI.java
+++ src/General/GUI.java
@@ -6,6 +6,15 @@ public class GUI {
 
     public static void main(String[] args) {
 
-        JFrame jFrame = new JFrame();
+        JFrame frame = new JFrame();
+
+        frame.setTitle("Login");
+        frame.setSize(300,200);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(new JFrame().EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        JPanel panel = new JPanel();
+
+
     }
 }
