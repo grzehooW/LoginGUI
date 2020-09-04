b091fef4eba3f5cfb9db3dfc37053a4fbc78dfea
grzehoo <grzehoo.wrzosek@gmail.com>
2020-09-04 12:00:52 +0100

    Created Lable, Text, and button fiields

--- src/General/GUI.java
+++ src/General/GUI.java
@@ -1,19 +1,36 @@
 package General;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Login Frame");
+
+        FlowLayout layout = new FlowLayout(FlowLayout.LEFT, 10, 20);
+
+        frame.setLayout(layout);
+
+        frame.add(new JLabel("User Name:"));
+        frame.add(new JTextField(10));
+        frame.add(new JLabel("Password: "));
+        frame.add(new JPasswordField(10));
+        frame.add(new JButton("Submit"));
+        frame.add(new JButton("Exit"));
+
+
 
         frame.setTitle("Login");
-        frame.setSize(300,200);
+        frame.setSize(250, 210);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(new JFrame().EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        JPanel panel = new JPanel();
+
+
+
+
 
 
     }
